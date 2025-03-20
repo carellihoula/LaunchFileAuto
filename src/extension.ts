69f36711aae11915ba62efd3a14b75efe94ff415
carellihoula <carellihoula10@gmail.com>
2025-03-20 10:23:23 +0100

    feat: add WebSocket support for file opening and enhance package dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,34 @@
 import * as vscode from "vscode";
+import WebSocket from "ws";
+
+let ws: WebSocket;
 
 export function activate(context: vscode.ExtensionContext) {
-  // Expose openFile function in the global context
-  (document as any).openFile = async (filePath: string) => {
-    try {
-      const uri = vscode.Uri.file(filePath);
-      const document = await vscode.workspace.openTextDocument(uri);
-      // Display the opened document in the VSCode editor
-      await vscode.window.showTextDocument(document);
-      console.log(`opened file : ${filePath}`);
-    } catch (error) {
-      console.error(`Unable to open this file : ${filePath}`, error);
-    }
-  };
+  ws = new WebSocket("ws://127.0.0.1:8000/ws");
 
-  console.log(" openFile extension is enabled.");
+  ws.on("open", () => {
+    console.log("​✅​ Connected to the WebSocket");
+  });
+
+  ws.on("message", (data: WebSocket.Data) => {
+    const message = data.toString();
+    console.log("Message brut reçu :", message);
+    const jsonData = JSON.parse(message);
+    if (jsonData.topic === "open_file") {
+      const filePath = jsonData.message;
+      vscode.window.showInformationMessage(`opened file : ${filePath}`);
+      vscode.workspace.openTextDocument(filePath).then((doc) => {
+        vscode.window.showTextDocument(doc);
+      });
+    }
+  });
+
+  ws.on("error", (error) => console.error("Error from WebSocket:", error));
+  ws.on("close", () => console.log("🚪 WebSocket is closed"));
 }
 
-export function deactivate() {}
+export function deactivate() {
+  if (ws) {
+    ws.close();
+  }
+}
